eb85f703c742893c366f31e3cc0a33574596b136
binary-arts application owner <tvj@vt.edu>
2017-08-13 17:12:43 +0000

    Initial modeling for performanceand composition

--- app/forms/hyrax/composition_form.rb
+++ app/forms/hyrax/composition_form.rb
@@ -3,6 +3,10 @@
 module Hyrax
   class CompositionForm < Hyrax::Forms::WorkForm
     self.model_class = ::Composition
-    self.terms += [:resource_type]
+    self.terms = [:composer, :title, :instruments, :description,
+                  :date, :tags, :length, :genre, :software,
+                  :medium]
+
+    self.required_fields = [:composer, :title, :instruments]
   end
 end
--- app/models/performance.rb
+++ app/models/performance.rb
@@ -3,9 +3,14 @@
 class Performance < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
   include ::Hyrax::BasicMetadata
+  include ::BinaryArts::Metadata
   # Change this to restrict which works can be added as a child.
   # self.valid_child_concerns = []
-  validates :title, presence: { message: 'Your work must have a title.' }
-  
+  validates :title, presence: { message: 'Your performance must have a title.' }
+  validates :performer, presence: { message: 'Your performance must have a performer.' }
+  validates :instruments, presence: { message: 'Your performance must have instruments.' }
+
   self.human_readable_type = 'Performance'
+
+  property :performer, predicate: ::RDF::Vocab::DC11.creator
 end
